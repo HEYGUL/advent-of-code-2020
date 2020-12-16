ec3ec0a708df22e85e837c67a953766260f4c636
Guillaume Lagorce <guillaume@hey-gul.com>
2020-12-16 22:29:08 +0100

    Add template folder and scripts to use it

---
+++ _template_/index.js
@@ -0,0 +1,12 @@
+const _ = require('lodash')
+
+module.exports = {
+
+  solvePart1 (input) {
+    const lines = input.split('\n')
+  },
+
+  solvePart2 (input) {
+    const lines = input.split('\n')
+  }
+}
