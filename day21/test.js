7b9389bd40f6ed7a3611398a171998bbe92a78a7
Guillaume Lagorce <guillaume@hey-gul.com>
2020-12-25 15:30:42 +0100

    Add day 21 (part 1)

--- day21/test.js
+++ day21/test.js
@@ -1,11 +1,14 @@
 const { solvePart1, solvePart2 } = require('./')
 
 test('solves part 1', () => {
-  const input = ''
+  const input = 'mxmxvkd kfcds sqjhc nhms (contains dairy, fish)\n' +
+      'trh fvjkl sbzzf mxmxvkd (contains dairy)\n' +
+      'sqjhc fvjkl (contains soy)\n' +
+      'sqjhc mxmxvkd sbzzf (contains fish)'
 
   const solution = solvePart1(input)
 
-  expect(solution).toEqual()
+  expect(solution).toEqual(5)
 })
 
 test('solves part 2', () => {
